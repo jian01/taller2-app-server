fcb8fd2c00f9fbea112458bae99ec4065e8c50f2
Gianmarco Cafferata <giancafferata@hotmail.com>
2020-07-11 20:04:19 -0300

    Changed to expo notification handling

--- src/database/notifications/postgres_expo_notification_database.py
+++ src/database/notifications/postgres_expo_notification_database.py
@@ -4,8 +4,7 @@ import json
 import psycopg2
 import os
 import logging
-import firebase_admin
-from firebase_admin import credentials, messaging
+import requests
 
 NOTIFICATION_TOKEN_SAVE = """
 DELETE FROM {notification_tokens_table_name}
@@ -23,24 +22,19 @@ FROM {notification_tokens_table_name}
 WHERE user_email = %s
 """
 
-class PostgresFirebaseNotificationDatabase:
+EXPO_SEND_NOTIFICATION_ENDPOINT = "https://exp.host/--/api/v2/push/send"
+
+NOTIFICATION_SEND_TIMEOUT = 5
+
+class PostgresExpoNotificationDatabase:
     """
     Notifications database
     """
     logger = logging.getLogger(__name__)
 
     def __init__(self, notification_tokens_table_name: str,
-                 firebase_credentials_json_env_name: str,
                  postgr_host_env_name: str, postgr_user_env_name: str,
                  postgr_pass_env_name: str, postgr_database_env_name: str):
-
-        creds = credentials.Certificate(json.loads(os.environ[firebase_credentials_json_env_name]))
-        firebase_admin.initialize_app(creds)
-        if firebase_admin.get_app():
-            self.logger.info("Connected to firebase")
-        else:
-            self.logger.error("Unable to connect to firebase")
-            raise ConnectionError("Unable to connect to firebase")
         self.notification_tokens_table_name = notification_tokens_table_name
         self.conn = psycopg2.connect(host=os.environ[postgr_host_env_name], user=os.environ[postgr_user_env_name],
                                      password=os.environ[postgr_pass_env_name],
@@ -68,39 +62,50 @@ class PostgresFirebaseNotificationDatabase:
         """
         self.logger.debug("Setting notification token for %s" % user_email)
         cursor = self.conn.cursor()
-        self.safe_query_run(self.conn, cursor,
-                            NOTIFICATION_TOKEN_SAVE.format(notification_tokens_table_name=self.notification_tokens_table_name),
-                            (user_email, token))
+        try:
+            self.safe_query_run(self.conn, cursor,
+                                NOTIFICATION_TOKEN_SAVE.format(notification_tokens_table_name=self.notification_tokens_table_name),
+                                (token, user_email, token))
+        except Exception:
+            self.logger.exception("Couldn't register notification token")
+            pass
         self.conn.commit()
         cursor.close()
 
-    def notify(self, user_email: str, payload: Dict) -> NoReturn:
+    def notify(self, user_email: str, title: str, body: str, payload: Dict) -> NoReturn:
         """
         Sends the payload a notification the the user if has registered an app token for that user
 
         :param user_email: the user email for sending the payload
+        :param title: the title of the notification
+        :param body: the body of the notification
         :param payload: the payload to send
         """
         self.logger.debug("Sending notification to %s" % user_email)
         cursor = self.conn.cursor()
-        self.safe_query_run(self.conn, cursor,
-                            SEARCH_NOTIFICATION_TOKEN.format(notification_tokens_table_name=self.notification_tokens_table_name),
-                            (user_email,))
-        result = cursor.fetchone()
+        try:
+            self.safe_query_run(self.conn, cursor,
+                                SEARCH_NOTIFICATION_TOKEN.format(notification_tokens_table_name=self.notification_tokens_table_name),
+                                (user_email,))
+            result = cursor.fetchone()
+        except Exception:
+            self.logger.exception("Couldn't send notification")
+            cursor.close()
+            return
         if result:
             token = result[0][0]
             cursor.close()
         else:
             cursor.close()
             return
-
-        message = messaging.Message(
-            data=payload,
-            token=token,
-        )
         try:
-            response = messaging.send(message)
-            self.logger.debug("Notification sent, id: %s" % response)
-        except Exception as err:
-            self.logger.exception("Notification couldn't be sent")
+            requests.post(EXPO_SEND_NOTIFICATION_ENDPOINT, json={"to": token,
+                                                                 "title": title,
+                                                                 "body": body,
+                                                                 "data": payload},
+                          timeout=NOTIFICATION_SEND_TIMEOUT)
+        except Exception:
+            self.logger.exception("Couldn't send notification")
             return
+
+
---
+++ test/src/database/notifications_database/test_expo_firebase_notification_database.py
@@ -0,0 +1,109 @@
+from src.database.notifications.postgres_expo_notification_database import PostgresExpoNotificationDatabase
+from src.database.statistics.statistics_database import ApiCall
+from datetime import datetime
+import pytest
+import psycopg2
+from typing import NamedTuple
+import requests
+import os
+from io import BytesIO
+
+class FakePostgres(NamedTuple):
+    closed: int
+
+class MockResponse:
+    def __init__(self, response):
+        self.response = response
+
+    def json(self):
+        return self.response
+
+    def raise_for_status(self):
+        return
+
+def create_mock_response(calls, response):
+    calls.append(response)
+    return MockResponse(response)
+
+@pytest.fixture(scope="function")
+def notifications_postgres_database(monkeypatch, postgresql):
+    os.environ["DUMB_ENV_NAME"] = "{}"
+    aux_connect = psycopg2.connect
+    monkeypatch.setattr(psycopg2, "connect", lambda *args, **kwargs: FakePostgres(0))
+    database = PostgresExpoNotificationDatabase(*(["DUMB_ENV_NAME"] * 5))
+    monkeypatch.setattr(psycopg2, "connect", aux_connect)
+    with open("test/src/database/notifications_database/config/initialize_db.sql", "r") as initialize_query:
+        cursor = postgresql.cursor()
+        cursor.execute(initialize_query.read())
+        postgresql.commit()
+        cursor.close()
+    database.conn = postgresql
+    database.notification_tokens_table_name = "chotuve.user_notification_tokens"
+    return database
+
+def test_postgres_connection_error(monkeypatch, notifications_postgres_database):
+    aux_connect = psycopg2.connect
+    monkeypatch.setattr(psycopg2, "connect", lambda *args, **kwargs: FakePostgres(1))
+    with pytest.raises(ConnectionError):
+        database = PostgresExpoNotificationDatabase(*(["DUMB_ENV_NAME"] * 5))
+    monkeypatch.setattr(psycopg2, "connect", aux_connect)
+
+def test_set_notification_tokens(monkeypatch, notifications_postgres_database):
+    notifications_postgres_database.set_notification_token('giancafferata@hotmail.com', "dummy")
+    notifications_postgres_database.set_notification_token('giancafferata@hotmail.com', "dummy1")
+    notifications_postgres_database.set_notification_token('cafferatagian@hotmail.com', "dummy2")
+    notifications_postgres_database.set_notification_token('asd@asd.com', "dummy3")
+    monkeypatch.setattr(PostgresExpoNotificationDatabase, "safe_query_run", AttributeError)
+    notifications_postgres_database.set_notification_token('giancafferata@hotmail.com', "dummy5")
+    cursor = notifications_postgres_database.conn.cursor()
+    cursor.execute("SELECT * FROM chotuve.user_notification_tokens")
+    results = cursor.fetchall()
+    for r in results:
+        if r[0] == 'giancafferata@hotmail.com':
+            assert r[1] == "dummy1"
+            continue
+        if r[0] == 'cafferatagian@hotmail.com':
+            assert r[1] == "dummy2"
+            continue
+        if r[0] == 'asd@asd.com':
+            assert r[1] == "dummy3"
+            continue
+        assert False
+
+def test_set_notification_tokens_delete_previous(monkeypatch, notifications_postgres_database):
+    notifications_postgres_database.set_notification_token('giancafferata@hotmail.com', "dummy1")
+    notifications_postgres_database.set_notification_token('cafferatagian@hotmail.com', "dummy1")
+    cursor = notifications_postgres_database.conn.cursor()
+    cursor.execute("SELECT * FROM chotuve.user_notification_tokens")
+    results = cursor.fetchall()
+    for r in results:
+        if r[0] == 'cafferatagian@hotmail.com':
+            assert r[1] == "dummy1"
+            continue
+        assert False
+
+def test_send_notification_no_token(monkeypatch, notifications_postgres_database):
+    calls = []
+    monkeypatch.setattr(requests, "post", lambda *args, **kwargs: create_mock_response(calls, {}))
+    notifications_postgres_database.notify('giancafferata@hotmail.com', "Hola", "Mundo", {})
+    assert len(calls) == 0
+
+def test_send_notification_query_run_exception(monkeypatch, notifications_postgres_database):
+    calls = []
+    monkeypatch.setattr(requests, "post", lambda *args, **kwargs: create_mock_response(calls, {}))
+    notifications_postgres_database.set_notification_token('giancafferata@hotmail.com', "dummy1")
+    monkeypatch.setattr(PostgresExpoNotificationDatabase, "safe_query_run", AttributeError)
+    notifications_postgres_database.notify('giancafferata@hotmail.com', "Hola", "Mundo", {})
+    assert len(calls) == 0
+
+def test_send_notification_post_exception(monkeypatch, notifications_postgres_database):
+    monkeypatch.setattr(requests, "post", AttributeError)
+    notifications_postgres_database.set_notification_token('giancafferata@hotmail.com', "dummy1")
+    notifications_postgres_database.notify('giancafferata@hotmail.com', "Hola", "Mundo", {})
+
+def test_send_notification_ok(monkeypatch, notifications_postgres_database):
+    calls = []
+    monkeypatch.setattr(requests, "post", lambda *args, **kwargs: create_mock_response(calls, {}))
+    notifications_postgres_database.set_notification_token('giancafferata@hotmail.com', "dummy1")
+    notifications_postgres_database.notify('giancafferata@hotmail.com', "Hola", "Mundo", {})
+    assert len(calls) == 1
